1dd74a0b51f3dd744f66ff6d095fb17eaabb3d48
Yehuda Ringler <guitarguy613@gmail.com>
2013-04-06 22:12:19 +0000

    Removed unnecessary comment

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,5 @@
 rm /usr/bin/dmenu_ltd_update.sh
 rm /usr/bin/dmenu_ltd_run.sh
 rm /usr/bin/dmenu_ltd_cat_run.sh
-# these "rm -r"s freak me out
 rm -r /usr/share/dmenu_ltd/
 rm -r /usr/share/doc/dmenu_ltd/
